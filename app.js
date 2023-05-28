4079bc6fda3f08a29b6071deabd05835f89bb310
Sadatul Islam Sadi <sadatulislamsadi@gmail.com>
2023-05-28 16:27:08 +0600

    Database support added. Now our posts don't get removed every time we close the server

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require('lodash');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://0.0.0.0:27017/DailyJournalDatabase");
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "title not set"]
+  },
+  body: {
+    type: String,
+    required: [true, "No discription"]
+  }
+});
+
+const postModel = mongoose.model("post", postSchema);
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -14,13 +28,16 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-let posts = [];
+// let posts = [];
 app.get("/", (req, res) => {
-  let shortPosts = posts.map((post) => ({
-    title: post.title,
-    body: post.body.substring(0, 100) + "..."
-  }));
-  res.render("home", { startingContent: homeStartingContent, posts: shortPosts });
+  postModel.find().then((posts) => {
+    let shortPosts = posts.map((post) => ({
+      id: post._id,
+      title: post.title,
+      body: post.body.substring(0, 100) + "..."
+    }));
+    res.render("home", { startingContent: homeStartingContent, posts: shortPosts });
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -35,36 +52,26 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.get("/posts/:postName", (req, res) => {
-  let searchedName = _.lowerCase(req.params.postName);
-  let flag = 0;
-  for (let i = 0; i < posts.length; i++) {
-    if (_.lowerCase(posts[i].title) === searchedName) {
-      flag = 1;
-      res.render("post", { post: posts[i] });
+app.get("/posts/:postId", (req, res) => {
+  postModel.findOne({ _id: req.params.postId }).then((result) => {
+    if (result) {
+      res.render("post", { post: result });
+    } else {
+      console.log("Match Not Found");
     }
-  }
-  if (flag == 0) console.log("Match Not Found");
+  });
 });
 
 app.post("/compose", (req, res) => {
-  let post = {
+  let post = new postModel({
     title: req.body.postTitle,
-    body: req.body.postBody
-  }
-  posts.push(post);
-  res.redirect("/");
-  // res.redirect("/compose");
+    body: req.body.postBody,
+  });
+  post.save().then(() => {
+    console.log("Upload Succesful");
+    res.redirect("/");
+  });
 });
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
